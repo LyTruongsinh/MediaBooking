c4f0d2b9cc9218457d6f62f5d09908b746133ffe
lytruongsinh123 <diepduong607@gmail.com>
2025-03-18 16:28:02 +0700

    change

--- Client/src/containers/System/UserManage.js
+++ Client/src/containers/System/UserManage.js
@@ -3,11 +3,13 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isOpenModel: false,
     };
   }
   /**
@@ -27,12 +29,32 @@ class UserManage extends Component {
     }
   }
 
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModel: true,
+    })
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModel: !this.state.isOpenModel,
+    })
+  }
   render() {
     console.log("check render", this.state);
     let arrUsers = this.state.arrUsers;
     return (
       <div className="users-container">
+        <ModalUser
+        isOpen = {this.state.isOpenModel}
+        toggleFromParent = {this.toggleUserModal}
+        />
         <div className="title text-center">MANAGE USER</div>
+        <div className="mx-5">
+          <button className="btn btn-primary px-3" onClick={() => this.handleAddNewUser()}>
+            <i className="fas fa-plus"></i> Add new user
+          </button>
+        </div>
         <div className="users-table mt-3 mx-5">
           <table id="customers">
             <tr>
@@ -42,7 +64,6 @@ class UserManage extends Component {
               <th>Address</th>
               <th>Actions</th>
             </tr>
-
             {arrUsers &&
               arrUsers.map((item, index) => {
                 return (
@@ -52,8 +73,12 @@ class UserManage extends Component {
                     <td>{item.lastName}</td>
                     <td>{item.address}</td>
                     <td>
-                        <button className="btn-edit"><i className="fas fa-edit"></i></button>
-                        <button className="btn-delete"><i className="fas fa-trash-alt"></i></button>
+                      <button className="btn-edit">
+                        <i className="fas fa-edit"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i className="fas fa-trash-alt"></i>
+                      </button>
                     </td>
                   </tr>
                 );
