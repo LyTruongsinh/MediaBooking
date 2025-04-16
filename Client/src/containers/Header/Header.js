6a5282fb4f33f65bc2134f7215ff3532a610a36a
lytruongsinh123 <diepduong607@gmail.com>
2025-04-16 22:22:46 +0700

    change

--- Client/src/containers/Header/Header.js
+++ Client/src/containers/Header/Header.js
@@ -5,10 +5,13 @@ import * as actions from "../../store/actions";
 import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
 import "./Header.scss";
-
+import { languages } from "../../utils/constant";
 class Header extends Component {
+  handleChangeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
   render() {
-    const { processLogout } = this.props;
+    const { processLogout, language } = this.props;
 
     return (
       <div className="header-container">
@@ -16,10 +19,27 @@ class Header extends Component {
         <div className="header-tabs-container">
           <Navigator menus={adminMenu} />
         </div>
-
-        {/* nút logout */}
-        <div className="btn btn-logout" onClick={processLogout}>
-          <i className="fas fa-sign-out-alt"></i>
+        <div className="languages">
+          <span
+            className={language === languages.VI ? "language-vi active" : "language-vi"}
+            onClick={() => this.handleChangeLanguage(languages.VI)}
+          >
+            VN
+          </span>
+          <span
+            className={language === languages.EN ? "language-en active" : "language-en"}
+            onClick={() => this.handleChangeLanguage(languages.EN)}
+          >
+            EN
+          </span>
+          {/* nút logout */}
+          <div
+            className="btn btn-logout"
+            onClick={processLogout}
+            title="Log out"
+          >
+            <i className="fas fa-sign-out-alt"></i>
+          </div>
         </div>
       </div>
     );
@@ -29,12 +49,14 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     processLogout: () => dispatch(actions.processLogout()),
+    changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language)),
   };
 };
 
--- Server/src/routes/web.js
+++ Server/src/routes/web.js
@@ -12,12 +12,13 @@ let initWebRouters = (app) => {
   router.post('/put-crud', homeController.putCRUD);
   router.get('/delete-crud', homeController.deleteCRUD);
 
-
   router.post('/api/login', userController.handleLogin);
   router.get('/api/get-all-users', userController.handleGetAllUsers);
   router.post('/api/create-new-user', userController.handleCreateNewUser);
   router.put('/api/edit-user', userController.handleEditUser);
   router.delete('/api/delete-user', userController.handleDeleteUser);
+  router.get('/allcode',  userController.getAllCode)
+
   return app.use("/", router);
 };
 module.exports = initWebRouters;
